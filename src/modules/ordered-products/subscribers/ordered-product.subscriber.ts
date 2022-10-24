c7e205f4b74084c72deb01b6f4ed1be192bfe338
Raam4 <ramiro_cardozo22@hotmail.com>
2022-10-24 16:19:06 -0300

    subscribers for stock movements

---
+++ src/modules/ordered-products/subscribers/ordered-product.subscriber.ts
@@ -0,0 +1,18 @@
+import { Product } from "modules/products/entities/product.entity";
+import { EntitySubscriberInterface, EventSubscriber, InsertEvent, UpdateEvent } from "typeorm";
+import { OrderedProduct } from "../entities/ordered-product.entity";
+
+@EventSubscriber()
+export class OrderedProductSubscriber implements EntitySubscriberInterface<OrderedProduct>{
+
+    listenTo(): any{
+        return OrderedProduct;
+    }
+
+    async afterInsert(event: InsertEvent<OrderedProduct>): Promise<any> {
+        const productRepository = event.manager.getRepository(Product);
+        const product = await productRepository.findOne({ where: event.entity.product});
+        product.stock -= event.entity.quantity;
+        await productRepository.save(product);
+    }
+}
